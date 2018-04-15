6b11bed4238409a611ee7487cc1a0bffddcea5e4
arshel <arshelmelfor@hotmail.com>
2018-04-15 15:26:48 +0200

    menu styling

--- menu.php
+++ menu.php
@@ -15,38 +15,41 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>garage menu</title>
+    <title>garage menu </title>
 </head>
 <body>
-<h1>garage menu</h1>
-<h2>klant</h2>
+<h1>garage menu<i class="fa fa-automobile" style="font-size:30px;"></i></h1>
+<h2>KLANT</h2>
+
 <div class="container">
-
-
+<div class="wrap">
 <ul>
     <li><a href="create-klant.php">create</a></li>
     <li><a href="read-klant.php">read</a></li>
-    <li><a href="zoek-klant.php">zoeken op klantid</a></li>
+    <li><a href="zoek-klant.php">zoek op klantid</a></li>
     <li><a href="update-klant.php">update</a></li>
     <li><a href="delete-klant.php">delete</a></li>
     <li><a href="read-klant-auto.php">read klant-auto</a></li>
 
 </ul>
 </div>
-<h2>auto</h2>
-<div class="container">
 
+
+    <h2>AUTO</h2>
+<div class="wrap">
 <ul>
+
     <li><a href="create-auto.php">create</a></li>
     <li><a href="read-auto.php">read</a></li>
-    <li><a href="zoek-auto.php">zoeken op autoid of type</a></li>
+    <li><a href="zoek-auto.php">zoek op autoid/type</a></li>
     <li><a href="update-auto.php">update</a></li>
     <li><a href="delete-auto.php">delete</a></li>
     <li><a href="read-klant-auto.php">read klant-auto</a></li>
 </ul>
-
+</div>
 </div>
 </body>
 </html>
